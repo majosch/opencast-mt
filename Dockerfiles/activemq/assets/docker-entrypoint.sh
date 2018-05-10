e19ea4eeea9a066b499ac9e6791cd4a679bcc89a
Martin Schamberger <martin.schamberger@univie.ac.at>
2018-05-10 18:06:16 +0200

    fix

--- Dockerfiles/activemq/assets/docker-entrypoint.sh
+++ Dockerfiles/activemq/assets/docker-entrypoint.sh
@@ -12,7 +12,9 @@ activemq_main_init() {
 #  activemq_tz_set
   sed -i -e "s/{{ACTIVEMQ_BROKER_USERNAME}}/$(echo $ACTIVEMQ_BROKER_USERNAME)/g" ${ACTIVEMQ_HOME}/conf/users.properties && \
   sed -i -e "s/{{ACTIVEMQ_BROKER_PASSWORD}}/$(echo $ACTIVEMQ_BROKER_PASSWORD)/g" ${ACTIVEMQ_HOME}/conf/users.properties && \
-  sed -i -e "s/{{ACTIVEMQ_BROKER_USERNAME}}/$(echo $ACTIVEMQ_BROKER_USERNAME)/g" ${ACTIVEMQ_HOME}/conf/groups.properties
+  sed -i -e "s/{{ACTIVEMQ_BROKER_USERNAME}}/$(echo $ACTIVEMQ_BROKER_USERNAME)/g" ${ACTIVEMQ_HOME}/conf/groups.properties && \
+  sed -i -e "s/127.0.0.1/0.0.0.0/g" ${ACTIVEMQ_HOME}/conf/activemq.xml && \
+  sed -i -e '/<\/shutdownHooks>/ r ${ACTIVEMQ_HOME}/conf/jaas-snippet.xml' ${ACTIVEMQ_HOME}/conf/activemq.xml
 
 }
 
